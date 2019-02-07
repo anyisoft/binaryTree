47d65630025b356ee0b22a8871cd034484153c95
leiwei <foo_lw@163.com>
2019-02-07 11:12:44 +0800

    添加线索二叉树算法

--- binaryTree/binaryTree/binaryTree.cpp
+++ binaryTree/binaryTree/binaryTree.cpp
@@ -20,6 +20,19 @@ struct tree_t {
 typedef struct tree_t treenode_t;
 typedef treenode_t *btree_t;
 
+/**
+ * 线索二叉树
+ */
+struct t_tree {
+	int data;             // 节点数据
+	int lefthread;        // 是否左线索
+	int righthread;       // 是否右线索
+	struct t_tree *left;  // 左子树
+	struct t_tree *right; // 右子树
+};
+typedef struct t_tree t_treenode;
+typedef t_treenode *t_btree;
+
 btree insertnode(btree root, int value)
 {
 	btree newnode;
@@ -229,22 +242,141 @@ void showTreeLeaf_inorder(btree ptr)
 	}
 }
 
+/**
+ * 插入线索二叉树的节点
+ */
+t_btree t_insertnode(t_btree root, int value)
+{
+	t_btree newnode;
+	t_btree current;
+	t_btree parent;
+	t_btree previous;
+	int     pos;
+
+	newnode = (t_btree)malloc(sizeof(treenode) );
+	newnode->data = value;
+	newnode->left = NULL;
+	newnode->right = NULL;
+	newnode->lefthread = 1;
+	newnode->righthread = 1;
+
+	current = root->right;
+	if (NULL == current) {
+		root->right = newnode;
+		newnode->left = root;
+		newnode->right = root;
+
+		return root;
+	}
+
+	parent = root;
+	pos = 0;
+	while (current != NULL) {
+		if (current->data > value) {
+			if (pos != -1) {
+				pos = -1;
+				previous = parent;
+			}
+			parent = current;
+			if (current->lefthread == 0 ) {
+				current = current->left;
+			}
+			else {
+				current = NULL;
+			}
+		}
+		else {
+			if (pos != 1) {
+				pos = 1;
+				previous = parent;
+			}
+			parent = current;
+			if (current->righthread == 0) {
+				current = current->right;
+			}
+			else {
+				current = NULL;
+			}
+		}
+	}
+	if (parent->data > value) {
+		parent->lefthread = 0;
+		parent->left = newnode;
+		newnode->left = previous;
+		newnode->right = parent;
+	}
+	else {
+		parent->righthread = 0;
+		parent->right =newnode;
+		newnode->left = parent;
+		newnode->right = previous;
+	}
+
+	return root;
+}
+
+/**
+ * 创建线索二叉树
+ */
+t_btree createtbtree(int *data, int len)
+{
+	t_btree root = NULL;
+	int i;
+
+	// 创建线索二叉树头节点
+	root = (t_btree)malloc(sizeof(t_treenode) );
+	root->left = root;
+	root->right = NULL;
+	root->lefthread = 1;
+	root->righthread = 0;
+
+	for (i = 0; i < len; i++) {
+		root = t_insertnode(root, data[i]);
+	}
+
+	return root;
+}
+
+/**
+ * 线索二叉树中序遍历输出
+ */
+void printbtree(t_btree root)
+{
+	t_btree ptr;
+
+	ptr = root;
+	do {
+		if (1 == ptr->righthread) {
+			ptr = ptr->right;
+		}
+		else {
+			ptr = ptr->right;
+			while (ptr->lefthread != 1) {
+				ptr = ptr->left;
+			}
+		}
+		if (ptr != root) {
+			printf("[%d]\n", ptr->data );
+		}
+	} while (ptr != root);
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	 btree root = NULL;
-	 int data[9] = { 5, 6, 4, 8, 2, 3, 7, 1, 9 };
-	 int nodeCount = 0;
-	 int height = 0;
+	// btree root = NULL;
+	int data[9] = { 5, 6, 4, 8, 2, 3, 7, 1, 9 };
+	// int nodeCount = 0;
+	// int height = 0;
 	
-	 root = createbtree(data, 9);
-	 // inorder(root);
-	 // preorder(root);
-	 // postorder(root);
-	 // nodeCount = calcNodeCount(root);
-	 // printf("node count is: %2d", nodeCount);
-	 // calcTreeHeight(root, 0, &height);
-	 // printf("node height is: %2d", height);
-	 showTreeLeaf_inorder(root);
+	// root = createbtree(data, 9);
+	// inorder(root);
+    // preorder(root);
+	// postorder(root);
+	// nodeCount = calcNodeCount(root);
+	// printf("node count is: %2d", nodeCount);
+	// calcTreeHeight(root, 0, &height);
+	// printf("node height is: %2d", height);
+	// showTreeLeaf_inorder(root);
 
 
 	// btree_t root = NULL;
@@ -253,6 +385,12 @@ int _tmain(int argc, _TCHAR* argv[])
 	// inorder_t(root);
 	// preorder_t(root);
 	// postorder_t(root);
+
+	t_btree root = NULL;
+
+	root = createtbtree(data, 9);
+	printf("thread tree's content: \n");
+	printbtree(root);
 
 	return 0;
 }
